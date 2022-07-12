f97cc357d228c51fd2f9a347754d49791004c855
Hisham Muhammad <hisham@gobolinux.org>
2022-07-12 13:52:04 -0300

    JSON integers are always decimal

--- lua_cjson.c
+++ lua_cjson.c
@@ -1176,7 +1176,7 @@ static int json_is_invalid_number(json_parse_t *json)
 static void json_next_number_token(json_parse_t *json, json_token_t *token)
 {
     char *endptr;
-    token->value.integer = strtoll(json->ptr, &endptr, 0);
+    token->value.integer = strtoll(json->ptr, &endptr, 10);
     if (json->ptr == endptr) {
         json_set_token_error(token, json, "invalid number");
         return;
